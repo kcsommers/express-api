3737c659cdae7a387c0dc831190b465f6bf484a7
Mark Sommers <kacysommers@gmail.com>
2018-06-13 22:47:27 -0700

    completed REST

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', 'ejs');
-app.use(bp.urlencoded({extended: false}));
+app.use(bp.urlencoded({extended: true}));
 app.use(ejsLayouts);
 
 // GET /songs - returns all songs
@@ -16,6 +16,53 @@ app.get('/songs', function(req, res) {
 	var songs = fs.readFileSync('./data.json');
 	songs = JSON.parse(songs);
 	res.json(songs);
+});
+
+// POST /songs - adds a new song
+app.post('/songs', function(req, res) {
+	var songs = fs.readFileSync('./data.json');
+	songs = JSON.parse(songs);
+	songs.push({name: req.body.name, album: req.body.album});
+	fs.writeFileSync('./data.json', JSON.stringify(songs));
+	res.json(songs);
+});
+
+//  GET /songs/:name - get 1 song
+app.get('/songs/:name', function(req, res) {
+	var songs = fs.readFileSync('./data.json');
+	songs = JSON.parse(songs);
+	if(songs[req.params.name]) {
+		res.json(songs[req.params.name]);
+	}
+	else {
+		res.send('No Can Doosville')
+	}
+});
+// PUT /songs/:name - update 1 song
+app.put('/songs/:name', function(req, res) {
+	var songs = fs.readFileSync('./data.json');
+	songs = JSON.parse(songs);
+	if(songs[req.params.name]) {
+		songs[req.params.name] = {name: req.body.name, album: req.body.album};
+		fs.writeFileSync('./data.json', JSON.stringify(songs));
+		res.json(songs);
+	}
+	else {
+		res.send('No Can Doosville')
+	}
+});
+// DELETE /songs/:name - delete 1 song
+app.delete('/songs/:name', function(req, res) {
+	var songs = fs.readFileSync('./data.json');
+	songs = JSON.parse(songs);
+	if(songs[req.params.name]) {
+		songs.splice(songs[req.params.name], 1);
+		fs.writeFileSync('./data.json', JSON.stringify(songs));
+		res.json(songs);
+	}
+	else {
+		res.send('No Can Doosville')
+	}
 });
 
 app.listen(3000);
